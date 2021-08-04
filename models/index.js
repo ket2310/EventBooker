25f78d6726cbbcd28598355b2e4d381620f5aa77
Kirk Thomas <dablumaroon@gmail.com>
2021-08-03 20:43:59 -0400

    Created models and associations.

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const Event  = require('./Event');
+const User  = require('./User');
+
+// ASSOCIATIONS:
+User.hasMany(Event, {
+    foreignKey: 'event_id',
+    onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(Poster, {
+    foreignKey: 'event_id'
+})
+
+
+
+module.exports = { Event, User}
